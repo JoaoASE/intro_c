1058eba1dd28321db4979cf3c4b2e330d23ec520
João Augusto <80292739+JoaoASE@users.noreply.github.com>
2023-08-24 16:57:45 -0300

    Create ex5.c

---
+++ ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  float temperatura_F, Celsius;
+
+  printf("\n Informe a temperatura na escala Fahrenheit: ");
+  scanf("%f", &temperatura_F);
+
+  Celsius = 0.555555555 * (temperatura_F - 32);
+  printf("\n Temperatura em graus Celsius=%.2f\n",Celsius); 
+
+  return 0;
+}
